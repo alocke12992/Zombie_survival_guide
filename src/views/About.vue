7ef416b7b35772fec4fd5a8c427f0bc3c694534a
Andrew Locke <alocke12992@gmail.com>
2018-11-01 16:18:52 -0700

    Minor style changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <h1>Works Cited</h1>
-    
+    <ul v-for="item in links" v-bind:key='item.id'>
+      <li>
+        <a :href='item.link' target='_blank'>
+        {{item.link}}
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import data from '../assets/data.js'
 export default {
-  name: 'About'
+  name: 'About',
+  data: function() {
+    return {
+      links: data.about
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  h1 {
+    text-align: center;
+  }
 </style>
