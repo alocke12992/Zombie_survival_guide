c8661e23836a9bb485b2ba050f637584834c0091
Andrew Locke <alocke12992@gmail.com>
2018-10-31 18:59:32 -0700

    Initial Commit

---
+++ src/layout/index.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="app-layout">
+    <AppContent/>
+  </div>
+</template>
+
+<script lang="ts">
+import AppContent from './AppContent.vue'
+
+export default {
+  name: 'appLayout',
+  components: {
+    AppContent
+  }
+}
+</script>
---
+++ src/views/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Zombie Survival Guide</h1>
+    <h3>Get Started before it's too late</h3>
+    <button>Get Started</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style>
+
+</style>
